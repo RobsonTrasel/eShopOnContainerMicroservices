c815d3008743d443d2c9b1af08870df21df26540
Robson Fernando Trasel de Souza <robson.trasel@gmail.com>
2023-01-18 20:10:24 -0300

    chore(setup): initial setup for project

---
+++ src/main/java/com/dev/eshopping/microservices/EShoppingContainerizedMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.dev.eshopping.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EShoppingContainerizedMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EShoppingContainerizedMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/eshopping/microservices/EShoppingContainerizedMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.eshopping.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EShoppingContainerizedMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
