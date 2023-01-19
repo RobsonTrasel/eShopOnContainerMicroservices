a819e983eac2e5379c30c1a8faa6f02281a958db
Robson Fernando Trasel de Souza <robson.trasel@gmail.com>
2023-01-19 00:44:45 -0300

    feat(api): create category entity

---
+++ src/api/src/main/java/com/dev/eshopping/microservices/domain/entities/Category.java
@@ -0,0 +1,14 @@
+package com.dev.eshopping.microservices.domain.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+}
